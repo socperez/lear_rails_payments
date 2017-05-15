e734da04f559c5e19858df3a0d24e77b97899291
Socrates Perez <socperezjr@gmail.com>
2017-05-15 06:25:22 -0700

    added the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,12 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 
 niceCat = Product.create(title: "Quality Kitty"
 	subtitle: "Learn App Code", image_name: "kitty.jpg",
-	price: "4.99", sku: "kittyOne", download_url: "https://s3-us-west-2.amazonaws.com/soc-learnapp-bucket/NightofWorship-43.jpg",
-	details: "A really nice cat", description %{<p>It's a cat</p>
+	price: "4.99", sku: "kittyOne", download_url: "https://s3-us-west-2.amazonaws.com/soc-learnapp-bucket/kitty.jpg",
+	details: "A really nice cat", description: %{<p>It's a cat</p>
+	})
+
+niceDog = Product.create(title: "Quality Dog",
+	subtitle: "Learn App Code", image_name: "dog.jpg",
+	price: "4.99", sku: "dogOne", download_url: "https://s3-us-west-2.amazonaws.com/soc-learnapp-bucket/dog.jpg",
+	details: "A really nice dog", description: %{<p>It's a dog</p>
 	})
